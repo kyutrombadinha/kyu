7a45db4a247b46a516a7862c2d49d056768797bb
kyutrombadinha <77802797+kyutrombadinha@users.noreply.github.com>
2021-01-26 03:10:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -476,11 +476,37 @@ async function starts() {
 					buffer = await getBuffer(anu.result)
 					client.sendMessage(from, buffer, image, {quoted: mek})
 					break
+				
+				case 'nfneko':
+					teks = body.slice(8)
+					/*if (!isDaftar) return reply(mess.only.daftarB)*/
+					reply(mess.wait)
+					anu = await fetchJson(`https://tobz-api.herokuapp.com/api/nsfwneko?apikey=BotWeA`, {method: 'get'})
+					buffer = await getBuffer(anu.result)
+					client.sendMessage(from, buffer, image, {quoted: mek})
+					break
+				case 'nfblow':
+					teks = body.slice(8)
+					/*if (!isDaftar) return reply(mess.only.daftarB)*/
+					reply(mess.wait)
+					anu = await fetchJson(`https://tobz-api.herokuapp.com/api/nsfwblowjob?apikey=BotWeA`, {method: 'get'})
+					buffer = await getBuffer(anu.result)
+					client.sendMessage(from, buffer, image, {quoted: mek})
+					break
 				case 'cry':
 					teks = body.slice(6)
 					/*if (!isDaftar) return reply(mess.only.daftarB)*/
-					reply(mess.wait)
+					//reply(mess.wait)
 					anu = await fetchJson(`https://tobz-api.herokuapp.com/api/cry?apikey=BotWeA`, {method: 'get'})
+					buffer = await getBuffer(anu.result)
+					client.sendMessage(from, buffer, image, {quoted: mek})
+					break
+					
+				case 'hug':
+					teks = body.slice(6)
+					/*if (!isDaftar) return reply(mess.only.daftarB)*/
+					//reply(mess.wait)
+					anu = await fetchJson(`https://tobz-api.herokuapp.com/api/hug?apikey=BotWeA`, {method: 'get'})
 					buffer = await getBuffer(anu.result)
 					client.sendMessage(from, buffer, image, {quoted: mek})
 					break
