c85a0639763c76047a5f84b2c093add40fcf7050
kyutrombadinha <77802797+kyutrombadinha@users.noreply.github.com>
2021-01-28 22:13:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -815,9 +815,9 @@ async function starts() {
 						} 
 						catch (e) 
 						{
-							//anu = await fetchJson(`https://tobz-api.herokuapp.com/api/hug?apikey=BotWeA`, {method: 'get'})
-							//buffer = await getBuffer(anu.result)
-							//client.sendMessage(from, buffer, image, {quoted: mek})
+							anu = await fetchJson(`https://tobz-api.herokuapp.com/api/hug?apikey=BotWeA`, {method: 'get'})
+							buffer = await getBuffer(anu.result)
+							client.sendMessage(from, buffer, image, {quoted: mek})
 							reply('âŒ *ERROR* âŒ')
 						}
 					break
