51435b29cc6601bf62eb87963efe01c8baef0085
kyutrombadinha <77802797+kyutrombadinha@users.noreply.github.com>
2021-01-29 01:39:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -829,6 +829,7 @@ async function starts() {
 						if (err) return reply(mess.error.stick)
 						buffer = fs.readFileSync(rano)
 						client.sendMessage(from, buffer, sticker, {quoted: mek})
+						reply('wait2')
 						fs.unlinkSync(rano)
 					})
 					break
