172b67e2642d9decf00d8373150a451a4ce6ead1
kyutrombadinha <77802797+kyutrombadinha@users.noreply.github.com>
2021-01-29 02:27:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -804,15 +804,6 @@ async function starts() {
 				case 'del':
 					client.deleteMessage(from, { id: mek.message.extendedTextMessage, remoteJid: from, fromMe: true })
 					break
-				case 'cry':
-					//teks = body.slice(6)
-					/*if (!isDaftar) return reply(mess.only.daftarB)*/
-					//reply(mess.wait)
-					anu = await fetchJson(`https://tobz-api.herokuapp.com/api/cry?apikey=BotWeA`, {method: 'get'})
-					buffer = await getBuffer(anu.result)
-					//client.sendMessage(from, buffer, image, {quoted: mek})
-					client.sendMessage(from, buffer, MessageType.video)
-					break
 				case 'baka':
 					teks = body.slice(6)
 					/*if (!isDaftar) return reply(mess.only.daftarB)*/
@@ -821,22 +812,40 @@ async function starts() {
 					client.sendMessage(from, './media/bakaa.mp3', audio, {quoted: mek})
 					break
 				case 'hug':
-					reply('wait')
-					try {
-						anu = await fetchJson(`https://tobz-api.herokuapp.com/api/hug?apikey=BotWeA`, {method: 'get'})
-						buffer = await getBuffer(anu.result)
-						client.sendMessage(from, buffer, image, {quoted: mek})
-						} 
-						catch (e) 
-						{
-							hug()
-						}
-					break
-				case 'hug2':
-					reply('wait2')
+					reply(mess.wait)
 					ranp = getRandom('.gif')
 					rano = getRandom('.webp')
 					anu = await fetchJson('https://tobz-api.herokuapp.com/api/hug?apikey=BotWeA', {method: 'get'})
+                                        //if (!isUser) return reply(mess.only.daftarB)
+					//if (anu.error) return reply(anu.error)
+					exec(`wget ${anu.result} -O ${ranp} && ffmpeg -i ${ranp} -vcodec libwebp -filter:v fps=fps=15 -lossless 1 -loop 0 -preset default -an -vsync 0 -s 512:512 ${rano}`, (err) => {
+						fs.unlinkSync(ranp)
+						//if (err) return reply(mess.error.stick)
+						buffer = fs.readFileSync(rano)
+						client.sendMessage(from, buffer, sticker, {quoted: mek})
+						fs.unlinkSync(rano)
+					})
+					break
+				case 'cry':
+					reply(mess.wait)
+					ranp = getRandom('.gif')
+					rano = getRandom('.webp')
+					anu = await fetchJson('https://tobz-api.herokuapp.com/api/cry?apikey=BotWeA', {method: 'get'})
+                                        //if (!isUser) return reply(mess.only.daftarB)
+					//if (anu.error) return reply(anu.error)
+					exec(`wget ${anu.result} -O ${ranp} && ffmpeg -i ${ranp} -vcodec libwebp -filter:v fps=fps=15 -lossless 1 -loop 0 -preset default -an -vsync 0 -s 512:512 ${rano}`, (err) => {
+						fs.unlinkSync(ranp)
+						//if (err) return reply(mess.error.stick)
+						buffer = fs.readFileSync(rano)
+						client.sendMessage(from, buffer, sticker, {quoted: mek})
+						fs.unlinkSync(rano)
+					})
+					break
+				case 'blowjb':
+					reply(mess.wait)
+					ranp = getRandom('.gif')
+					rano = getRandom('.webp')
+					anu = await fetchJson('https://tobz-api.herokuapp.com/api/nsfwblowjob?apikey=BotWeA', {method: 'get'})
                                         //if (!isUser) return reply(mess.only.daftarB)
 					//if (anu.error) return reply(anu.error)
 					exec(`wget ${anu.result} -O ${ranp} && ffmpeg -i ${ranp} -vcodec libwebp -filter:v fps=fps=15 -lossless 1 -loop 0 -preset default -an -vsync 0 -s 512:512 ${rano}`, (err) => {
