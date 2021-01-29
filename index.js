991a87d9d4efd6e6bcdd8d65c2fa7c6e6d7a9fce
kyutrombadinha <77802797+kyutrombadinha@users.noreply.github.com>
2021-01-29 01:46:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -826,7 +826,7 @@ async function starts() {
 					//if (anu.error) return reply(anu.error)
 					exec(`wget ${anu.result} -O ${ranp} && ffmpeg -i ${ranp} -vcodec libwebp -filter:v fps=fps=15 -lossless 1 -loop 0 -preset default -an -vsync 0 -s 512:512 ${rano}`, (err) => {
 						fs.unlinkSync(ranp)
-						if (err) return reply(mess.error.stick)
+						//if (err) return reply(mess.error.stick)
 						buffer = fs.readFileSync(rano)
 						client.sendMessage(from, buffer, sticker, {quoted: mek})
 						fs.unlinkSync(rano)
