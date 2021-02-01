c32b662589e609af7076a61e8b9e5993efc9dc43
kyutrombadinha <77802797+kyutrombadinha@users.noreply.github.com>
2021-01-31 23:37:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1371,13 +1371,13 @@ async function starts() {
 								ranm = getRandom('.mp3')
 								rano = getRandom('.ogg')
 								var numero = getRandomIntInclusive(0,100).toString()
-								var nome = './mp3/loli (' + numero+').mp3'
+								var nome = './mp3/loli (' + numero +').mp3'
 								const buffer6 = fs.readFileSync(nome)
 								ranm = fs.readFileSync(nome)
 								exec(`ffmpeg -i ${ranm} -ar 48000 -vn -c:a libopus ${rano}`, (err) => {
 									fs.unlinkSync(ranm)
 									buff = fs.readFileSync(rano)
-								client.sendMessage(from, buff, audio, {quoted: mek, ptt:true})
+								client.sendMessage(from, buff, audio, {quoted: mek, ptt:true})}
 								break
 						}
 							console.log(color('[ERROR]','red'), 'Unregistered Command from', color(sender.split('@')[0]))
